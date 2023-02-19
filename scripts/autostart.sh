3d5ca70fd8a67ec9cfdfd99e57353ed954741111
Ankush Ahuja <aahuja2006@gmail.com>
2023-02-19 16:02:18 +0100

    New Border and add Screenshot tool. Change EWW colors

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -7,5 +7,7 @@ $SCRIPTS/utils/wallpaper.sh $HOME/Pictures/Wallpaper/Wallpaper &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 # Waybar
 waybar -c $HYPR/ui/waybar/config -s $HYPR/ui/waybar/style.css &
+# Notification Daemon
+dunst -config $HYPR/ui/dunst/dunstrc &
 # EWW Daemon
 $SCRIPTS/apps/eww.sh daemon &
--- scripts/utils/brightness.sh
+++ scripts/utils/brightness.sh
@@ -3,7 +3,7 @@
 ICONDIR=$HOME/.config/hypr/ui/icons/aether/dark
 
 function cbright {
-    brightnessctl i | grep '%' | awk -F'(' '{print $2}' | awk -F'%' '{print $1}'
+  light | awk '{print int($1+0.5)}'
 }
 
 function notify {
@@ -30,22 +30,21 @@ function notify {
             fi
         fi
     fi
-    echo "$brightness" > ~/.config/i3/scripts/brightness-current
     notify-send -i "$icon_name.png" -t 2000 -r 1234 "$brightness" "Brightness"
 }
 
 case $1 in
     up)
-	brightnessctl set 2%+ > /dev/null
+	light -A 5
 	notify
 	;;
     down)
-    brightnessctl set 2%- > /dev/null
+  light -U 5
 	notify
 	;;
     optimal)
     # Toggle optimal
-	brightnessctl set 25% > /dev/null
+	  light -S 50
     notify
 	;;
 esac     
